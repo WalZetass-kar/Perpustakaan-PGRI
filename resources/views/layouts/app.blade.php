01800a5eda303875753754718fbfcc3012e72658
WalZetass-kar <mihwalmaulana09@gmail.com>
2026-08-12 10:52:24 -0400

    Fix modal auto-opening flash by adding global x-cloak CSS rule to hide popups before AlpineJS initializes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
     
     <style>
+        [x-cloak] { display: none !important; }
         html { scroll-behavior: smooth; }
         body { font-family: 'Poppins', sans-serif; overflow-x: hidden; }
     </style>
